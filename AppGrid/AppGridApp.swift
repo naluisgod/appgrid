d5cba8a8a8efd18d3af71d26ef5447d9524f4353
Laurent Lenert <nalu@lalencrypt.com>
2026-01-16 17:42:17 +0100

    Initial Commit

---
+++ AppGrid/AppGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppGridApp.swift
+//  AppGrid
+//
+//  Created by Laurent Lenert on 16/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct AppGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
